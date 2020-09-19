83817980cc5c86a405a80272abfb56b861251d97
Isabella Baum <isabella.baum@gmx.net>
2020-09-19 02:40:53 +0200

    add basic follower AI

---
+++ Assets/Scripts/DragonMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonMovement : MonoBehaviour
+{
+
+	private Transform player;
+	public float movementSpeed = 10f;
+	private float minFollowDistance = 7.5f;
+
+	void Start() {
+		player = GameObject.FindWithTag("Player").transform;
+	}
+
+    void Update()
+    {
+    	transform.LookAt(player);
+
+    	if(Vector3.Distance(player.position, transform.position) > minFollowDistance) {
+    		transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+    	}
+    }
+}
