788ea86a8d045f9cf99a055b589ad8ba77fb31a0
Isabella Baum <isabella.baum@gmx.net>
2020-09-22 16:29:04 +0200

    add basic hotbar with no functionality

---
+++ Assets/Scripts/Hotbar/HotbarUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotbarUI : MonoBehaviour
+{
+    //Hotbar hotbar;
+	//HotbarSlot[] slots;
+	public Transform slotHolder;
+}
